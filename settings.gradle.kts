041c091e9f8e2d9f998a12a61457fb34c3665622
Enrico Martelli <mrcnrt95e30@gmail.com>
2024-01-29 15:46:56 +0100

    Migrate to gradle/actions/setup-gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,18 +14,5 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("com.gradle.enterprise") version "3.16.2"
-}
-
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-
-        publishAlwaysIf(providers.environmentVariable("CI").isPresent)
-    }
-}
-
 rootProject.name = "Where's My Car"
 include(":app")
